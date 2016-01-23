07ae02d5e92df20886022bbaf6476d16ca680775
Pyet <poyetmelvin.pro@gmail.com>
2016-01-23 16:50:22 +0100

    Fini le chapitre HTML

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+#from blog.forms import ContactForm
+from django.views.generic.edit import FormView
+
+class ContactView(FormView):
+    template_name = 'contact.html'
+    #form_class = ContactForm
+    success_url = '/thanks/'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_email()
+        return super(ContactView, self).form_valid(form)
